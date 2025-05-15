1320f8359f49bb331754ffd4d46120f4ad647e42
kaayell <kaayell@gmail.com>
2025-05-15 11:45:53 -0500

    replace gsap with motion
    + add template.tsx to wrap pages in animation
    + remove TypewriterText functionality for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,20 @@
-import TypewriterText from "@/components/ui/TypewriterText";
-import { courierPrime, fredericka, lavishlyYours } from "@/lib/fonts";
+import {courierPrime, fredericka, lavishlyYours} from "@/lib/fonts";
 
 export default async function Home() {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <section className="flex items-center justify-center">
-        <div className="container-custom mt-20 ml-10">
-          <div className={`text-8xl font-bold mb-6 ${courierPrime.className}`}>
-            <TypewriterText text="kaay•ell" delay={0.15} speed={0.2} />
-            <hr className="border-neutral-700" />
-          </div>
-          <div className="flex flex-row items-baseline space-x-3">
-            <h2 className={fredericka.className}>KL</h2>
-            <h2 className={lavishlyYours.className}>kristie-lynne</h2>
-          </div>
-        </div>
-      </section>
-    </div>
-  );
+	return (
+		<div className="flex flex-col min-h-screen">
+			<section className="flex items-center justify-center">
+				<div className="container-custom mt-20 ml-10">
+					<div className={`text-8xl font-bold mb-6 ${courierPrime.className}`}>
+						<h4>kaay•ell</h4>
+						<hr className="border-neutral-700"/>
+					</div>
+					<div className="flex flex-row items-baseline space-x-3">
+						<h2 className={fredericka.className}>KL</h2>
+						<h2 className={lavishlyYours.className}>kristie-lynne</h2>
+					</div>
+				</div>
+			</section>
+		</div>
+	);
 }
