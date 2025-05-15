8bcf0238c5d918a0194a72967e1e275b362a88b9
kaayell <kaayell@gmail.com>
2025-05-15 15:09:27 -0500

    add TypewriterText.tsx back in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import {courierPrime, fredericka, lavishlyYours} from "@/lib/fonts";
+import TypewriterText from "@/components/ui/TypewriterText";
 
 export default async function Home() {
 	return (
@@ -6,7 +7,7 @@ export default async function Home() {
 			<section className="flex items-center justify-center">
 				<div className="container-custom mt-20 ml-10">
 					<div className={`text-8xl font-bold mb-6 ${courierPrime.className}`}>
-						<h4>kaay•ell</h4>
+						<TypewriterText text={"kaay•ell"} speed={200} delay={500} />
 						<hr className="border-neutral-700"/>
 					</div>
 					<div className="flex flex-row items-baseline space-x-3">
