d4a8f9056d66df278362e13265cb7497e44f2e2c
kaayell <kaayell@gmail.com>
2025-05-19 10:46:16 -0500

    noh noh typewriter for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import { courierPrime, fredericka, lavishlyYours } from "@/lib/fonts";
-import TypewriterText from "@/components/ui/TypewriterText";
 
 export default async function Home() {
   return (
-    <section className="flex flex-col items-center justify-center">
-      <div className="mx-auto px-4 sm:px-6 lg:px-8 mt-20 ml-10">
-        <div className={`text-8xl font-bold mb-6 ${courierPrime.className}`}>
-          <TypewriterText text={"kaay•ell"} speed={200} delay={500} />
-          <hr className="border-neutral-700" />
-        </div>
+    <section className="flex flex-col justify-center">
+      <div className="mr-auto px-4 sm:px-6 lg:px-8 mt-20">
+        <h4 className={`text-7xl font-bold mb-2 ${courierPrime.className}`}>
+          kaay•ell
+        </h4>
         <div className="flex flex-row items-baseline space-x-3">
           <h2 className={fredericka.className}>KL</h2>
           <h2 className={lavishlyYours.className}>kristie-lynne</h2>
