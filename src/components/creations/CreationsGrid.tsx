cff8250865902bdd09621dc1cb91f140ba91c3eb
kaayell <kaayell@gmail.com>
2025-06-27 13:22:08 -0500

    align content with holes

--- src/components/creations/CreationsGrid.tsx
+++ src/components/creations/CreationsGrid.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import { CloudinaryImage } from "@/lib/cloudinary";
+import { useMemo } from "react";
+import { CldImage } from "next-cloudinary";
+import { useRouter } from "next/navigation";
+import _ from "lodash";
 import { motion } from "motion/react";
-import { useEffect, useMemo, useState } from "react";
 import {
   createStaggerItem,
   fadeInUp,
+  scaleOnHover,
   staggerContainer,
 } from "@/lib/animations";
-import { CldImage } from "next-cloudinary";
-import { useRouter } from "next/navigation";
 
 interface CreationsGridProps {
   creations: CloudinaryImage[];
@@ -17,78 +19,36 @@ interface CreationsGridProps {
 
 export default function CreationsGrid({ creations }: CreationsGridProps) {
   const router = useRouter();
-  const [gridSize, setGridSize] = useState({ cols: 0, rows: 0 });
-  const SPACING = 100;
-
-  useEffect(() => {
-    const updateSize = () => {
-      const container = document.getElementById("pegboard-content");
-      if (container) {
-        const rect = container.getBoundingClientRect();
-        setGridSize({
-          cols: Math.floor(rect.width / SPACING),
-          rows: Math.floor(rect.height / SPACING),
-        });
-      }
-    };
-    updateSize();
-    window.addEventListener("resize", updateSize);
-    return () => {
-      window.removeEventListener("resize", updateSize);
-    };
-  }, []);
 
   const creationsWithSpans = useMemo(() => {
-    return creations.map((image) => {
-      const aspectRatio = image.width / image.height;
-      let gridSpan = [1, 1];
-      if (aspectRatio > 1.5) {
-        gridSpan = [1, 2]; // wide images
-      } else if (aspectRatio < 0.7) {
-        gridSpan = [2, 1]; // tall images
-      }
-
-      return {
-        ...image,
-        gridSpan: { col: gridSpan[1], row: gridSpan[0] },
-      };
-    });
+    return creations.map((image) => ({
+      ...image,
+      gridSpan: { col: 3, row: 4 },
+      totalFootprint: 3 * 4,
+    }));
   }, [creations]);
 
-  const pegHoles = useMemo(() => {
-    if (gridSize.cols === 0 || gridSize.rows === 0) {
-      return [];
-    }
-    const holeCount = gridSize.cols * gridSize.rows;
-    return [...new Array(holeCount)].map((value, index) => {
-      return {
-        id: `peg-${index}`,
-      };
-    });
-  }, [gridSize]);
+  const holeCount = useMemo(() => {
+    return _.sumBy(creationsWithSpans, (creation) => creation.totalFootprint);
+  }, [creationsWithSpans]);
 
   return (
     <div className="h-full pt-24 px-8 md:px-18 lg:px-26">
-      <div className="relative w-full h-full p-10 rounded-2xl pegboard">
+      <div className="relative min-h-screen w-full p-10 rounded-2xl pegboard">
         <div className="screw top-5 left-5" />
         <div className="screw top-5 right-5" />
         <div className="screw bottom-5 left-5" />
         <div className="screw bottom-5 right-5" />
-        <div id="pegboard-content" className="relative w-full h-full">
-          <div
-            className="pegboard-holes-grid"
-            style={{
-              gridTemplateColumns: `repeat(${gridSize.cols}, 1fr)`,
-              gridTemplateRows: `repeat(${gridSize.rows}, 1fr)`,
-            }}
-          >
-            {pegHoles.map((hole) => (
-              <div key={hole.id} className="pegboard-hole" />
-            ))}
+
+        <div className="relative w-full h-full">
+          <div className="w-full h-full pegboard-grid">
+            {[...new Array(holeCount)].map((_, index) => {
+              return <div key={`peg-${index}`} className="pegboard-hole" />;
+            })}
           </div>
 
           <motion.div
-            className="grid-gallery"
+            className="pegboard-grid absolute inset-0"
             variants={staggerContainer}
             initial={"initial"}
             animate={"animate"}
@@ -96,27 +56,27 @@ export default function CreationsGrid({ creations }: CreationsGridProps) {
             {creationsWithSpans.map((image, index) => (
               <motion.div
                 key={image.public_id}
-                layoutId={`image-${image.public_id}`}
-                className="group cursor-pointer relative gallery-item"
+                className="group cursor-pointer relative mt-10"
                 style={{
                   gridColumn: `span ${image.gridSpan.col}`,
                   gridRow: `span ${image.gridSpan.row}`,
                 }}
                 variants={createStaggerItem(fadeInUp)}
+                {...scaleOnHover}
                 onClick={() => router.push(`/creations/${image.public_id}`)}
               >
                 <div className="peg-hook" />
                 <div className="hanging-string" />
                 <CldImage
-                  key={index}
+                  key={`${image.public_id}-${index}`}
                   src={image.public_id}
                   alt={image.display_name}
                   width={image.width}
                   height={image.height}
-                  className="w-full h-full object-contain z-3"
+                  className="w-full h-full object-contain z-10"
                   style={{
                     filter:
-                      "drop-shadow(3px 4px 12px rgba(0, 0, 0, 0.50)) drop-shadow(1px 2px 6px rgba(0, 0, 0, 0.20));",
+                      "drop-shadow(3px 4px 12px rgba(0, 0, 0, 0.50)) drop-shadow(1px 2px 6px rgba(0, 0, 0, 0.20))",
                   }}
                 />
               </motion.div>
